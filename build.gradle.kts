af4d3e58b01f97c621e688acf9db3c7542f80265
Gustavo Massuela <ggmassuela@gmail.com>
2021-07-25 23:12:43 -0300

    added clientSideStreaming, serverSideStreaming and bidirectionalStreaming

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import com.google.protobuf.gradle.*
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    id("org.jetbrains.kotlin.kapt") version "1.4.32"
+    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+    id("org.jetbrains.kotlin.kapt") version "1.5.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("io.micronaut.application") version "1.5.0"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.32"
-    id("org.jetbrains.kotlin.plugin.jpa") version "1.4.32"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.5.0"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.5.0"
     id("com.google.protobuf") version "0.8.15"
 }
 
@@ -27,23 +27,25 @@ micronaut {
 
 dependencies {
     kapt("io.micronaut.data:micronaut-data-processor")
+    implementation("io.grpc:grpc-kotlin-stub:1.1.0")
+    implementation("io.micronaut:micronaut-validation")
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
     implementation("io.micronaut.grpc:micronaut-grpc-runtime")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.sql:micronaut-jdbc-hikari")
-    implementation("javax.annotation:javax.annotation-api")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:${kotlinVersion}")
+    implementation("javax.annotation:javax.annotation-api")
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:testcontainers")
-    implementation("io.micronaut:micronaut-validation")
-    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("org.mockito:mockito-core")
     testImplementation("io.micronaut:micronaut-http-client")
+    testImplementation("org.mockito:mockito-core")
 }
 
 
@@ -72,6 +74,7 @@ sourceSets {
     main {
         java {
             srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/grpckt")
             srcDirs("build/generated/source/proto/main/java")
         }
     }
@@ -79,11 +82,14 @@ sourceSets {
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.14.0"
+        artifact = "com.google.protobuf:protoc:3.17.0"
     }
     plugins {
         id("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.33.1"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:0.1.5"
         }
     }
     generateProtoTasks {
@@ -91,6 +97,7 @@ protobuf {
             it.plugins {
                 // Apply the "grpc" plugin whose spec is defined above, without options.
                 id("grpc")
+                id("grpckt")
             }
         }
     }
--- src/main/kotlin/com/services/DemoGrpcService.kt
+++ src/main/kotlin/com/services/DemoGrpcService.kt
@@ -1,4 +1,34 @@
 package com.services
 
-class DemoGrpcService {
+import com.example.ResponseTemplate
+import com.example.RequestTemplate
+import com.example.DemoGrpcServiceGrpcKt
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import javax.inject.Singleton
+
+@Singleton
+class DemoGrpcService : DemoGrpcServiceGrpcKt.DemoGrpcServiceCoroutineImplBase() {
+    override suspend fun send(request: RequestTemplate): ResponseTemplate {
+        return ResponseTemplate.newBuilder().setMessage("Hi, your message is: ${request.name}").build()
+    }
+
+    override suspend fun clientSideStreaming(requests: Flow<RequestTemplate>): ResponseTemplate {
+        return ResponseTemplate.newBuilder().setMessage("TESTE").build()
+    }
+
+    override fun serverSideStreaming(request: RequestTemplate): Flow<ResponseTemplate> {
+        return flow {
+            emit(ResponseTemplate.newBuilder().setMessage("TESTE").build())
+        }
+    }
+
+    override fun bidirectionalStreaming(requests: Flow<RequestTemplate>): Flow<ResponseTemplate> {
+        return flow {
+            requests.collect {
+                emit(ResponseTemplate.newBuilder().setMessage("TESTE").build())
+            }
+        }
+    }
 }
