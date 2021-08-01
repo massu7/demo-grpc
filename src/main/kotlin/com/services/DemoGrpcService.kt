4cad796babfe6f441404aa7f58e2f1eb40ba598a
Gustavo Massuela <ggmassuela@gmail.com>
2021-08-01 19:45:45 -0300

    Refactory clientSiderStreaming, severSideStreaming and bidirectionalStreaming

--- src/main/kotlin/com/services/DemoGrpcService.kt
+++ src/main/kotlin/com/services/DemoGrpcService.kt
@@ -3,31 +3,32 @@ package com.services
 import com.example.ResponseTemplate
 import com.example.RequestTemplate
 import com.example.DemoGrpcServiceGrpcKt
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import javax.inject.Singleton
 
 @Singleton
 class DemoGrpcService : DemoGrpcServiceGrpcKt.DemoGrpcServiceCoroutineImplBase() {
-    override suspend fun send(request: RequestTemplate): ResponseTemplate {
+    override suspend fun unaryCall(request: RequestTemplate): ResponseTemplate {
         return ResponseTemplate.newBuilder().setMessage("Hi, your message is: ${request.name}").build()
     }
 
     override suspend fun clientSideStreaming(requests: Flow<RequestTemplate>): ResponseTemplate {
-        return ResponseTemplate.newBuilder().setMessage("TESTE").build()
+        val requestsList = mutableListOf<String>()
+        requests.collect { item -> requestsList.add(item.name) }
+
+        return ResponseTemplate.newBuilder().setMessage("Hi, your message is: ${requestsList.toString()}").build()
     }
 
     override fun serverSideStreaming(request: RequestTemplate): Flow<ResponseTemplate> {
         return flow {
-            emit(ResponseTemplate.newBuilder().setMessage("TESTE").build())
+            emit(ResponseTemplate.newBuilder().setMessage("Hi, your message is: ${request.name}").build())
         }
     }
 
     override fun bidirectionalStreaming(requests: Flow<RequestTemplate>): Flow<ResponseTemplate> {
         return flow {
-            requests.collect {
-                emit(ResponseTemplate.newBuilder().setMessage("TESTE").build())
+            requests.collect { item ->
+                emit(ResponseTemplate.newBuilder().setMessage("Hi, your message is: ${item.name}").build())
             }
         }
     }
